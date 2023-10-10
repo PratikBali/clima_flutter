087290b38392d6211d6d83369d929d456b5381a0
Pratik Bali <pratikbali96@gmail.com>
2023-10-10 15:19:45 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -9,11 +9,14 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    LocationPermission permission = await Geolocator.checkPermission();
-    print('permission: $permission');
-
-    LocationPermission permReq = await Geolocator.requestPermission();
-    print('permReq: $permReq');
+    // LocationPermission permission = await Geolocator.checkPermission();
+    // print('permission: $permission');
+    //
+    // List<String> perm = permission.toString().split('.');
+    // print('perm: ${perm[1]}');
+    //
+    // LocationPermission permReq = await Geolocator.requestPermission();
+    // print('permReq: $permReq');
 
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
     print('position: $position');
